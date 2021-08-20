620a117f4958b25fb074e4ac6ba3c21ef35274cb
Alexander Santos <lekxandy@hotmail.com>
2021-08-20 20:34:35 -0300

    Added swagger ui, documentation for API

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerFile = require('./swagger_output.json');
+
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
@@ -11,6 +14,9 @@ app.use(cors());
 
 //Rotas da API
 app.use('/buscacep', require('./routes'));
+
+//Rota da documentação da API
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 //Execução do servidor
 app.listen(PORT, () => {
---
+++ backend/swagger.js
@@ -0,0 +1,9 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const outputFile = './swagger_output.json'
+
+const endpointsFiles = ['./routes.js']
+
+swaggerAutogen(outputFile, endpointsFiles).then(()=> {
+    require('./index')
+})
