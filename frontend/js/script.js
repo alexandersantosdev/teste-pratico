513ecfd6dd3662aada76fa810fd7031de845b677
Alexander Roberto dos Santos <84203330+alexandersantosdev@users.noreply.github.com>
2021-08-21 12:10:51 -0300

    Inserido evento tecla enter

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -4,21 +4,32 @@ const ceps = []
 
 window.onload = reset();
 
-if (ceps.length == 0){
+if (ceps.length == 0) {
     $("#dados").html("Nenhum CEP");
 }
 
 $("button").click(() => {
+    pesquisaCep();
+
+});
+
+$(document).keyup( e => {
+    if ($("#cep_pesquisa").is(":focus") && (e.keyCode == 13)) {
+        pesquisaCep();
+    }
+});
+
+function pesquisaCep() {
     $("#spinner").show()
     $("button").prop("disabled", true)
-    
+
     const cep = $("#cep_pesquisa").val().replace(/[\.-]/g, '');
-    
+
     if (cep == "" || cep == null || isNaN(cep) || cep.length < 8) {
-        
+
         $("#message").show();
         $("#message").html("Cep inválido, preencha o campo corretamente");
-        
+
     } else {
         $.ajax({
             url: `${API_URL}/${cep}`, async: true, success: function (response, _) {
@@ -27,26 +38,25 @@ $("button").click(() => {
             },
             error: (response, s) => {
                 console.log(response.statusText)
-                if(response.statusText == "error"){
+                if (response.statusText == "error") {
                     $("#message").show();
                     $("#message").html("Erro ao requisitar dados da API");
-         
-                }else{
+
+                } else {
                     $("#message").show();
                     $("#message").html(response.responseJSON.message);
                 }
             }
         });
-        
+
     }
     setTimeout(() => reset(), 2000)
     return
-    
-});
+}
 
-function populateTable(){
-    
-    const dado = ceps.map(c => 
+function populateTable() {
+
+    const dado = ceps.map(c =>
         `<tr>
         <td>${c.logradouro}</td>
         <td>${c.bairro}</td>
@@ -55,15 +65,15 @@ function populateTable(){
         <td>${c.ibge}</td>
         <td>${c.ddd}</td>
         </tr>`)
-        
-        $("#dados").html(dado);
-    }
-    function reset() {
-        
-        $("#spinner").hide()
-        $("#message").hide();
-        $("#cep_pesquisa").val("")
-        $("#cep_pesquisa").focus();
-        $("button").prop("disabled", false)
-    
+
+    $("#dados").html(dado);
+}
+function reset() {
+
+    $("#spinner").hide()
+    $("#message").hide();
+    $("#cep_pesquisa").val("")
+    $("#cep_pesquisa").focus();
+    $("button").prop("disabled", false)
+
 }
